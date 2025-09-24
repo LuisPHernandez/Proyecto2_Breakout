d8b35dc6eda7eb483eea1b1813226d56bd521300
LuisPHernandez <lphm@gmail.com>
2025-09-24 14:17:25 +0000

    Estructura del juego

--- src/game.cpp
+++ src/game.cpp
@@ -1,21 +1,11 @@
-#include <ncurses.h>
-#include <string>
+// LUPA
 
-// Estructura del estado actual del juego
-struct GameState {
-    // Geometría
-    int width, height;
-    float ball_x, ball_y, ball_vx, ball_vy;
-    int paddle_x, paddle_y, paddle_w;
+// LUPA
 
-    // Puntuación/vidas
-    int score = 0, lives = 3, level = 1;
-    bool running = true, paused = false, gameover = false, need_redraw = true;
+// JOEL
 
-    // Sincronización
-    pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
-    pthread_cond_t  cv_tick = PTHREAD_COND_INITIALIZER;   // marca de avance de frame
-    pthread_barrier_t frame_barrier;                      // sync fin de frame
-    sem_t event_sem;                                      // eventos (audio/log)
-    InputState input;
-};
+// JOEL
+
+// MIGUEL
+
+// MIGUEL
